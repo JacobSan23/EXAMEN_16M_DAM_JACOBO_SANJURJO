27fda4e04052fc2c706dc9b8d16dde92c97d0b7a
Acer <Acer@Felix>
2024-05-22 12:10:22 +0200

    UNO

---
+++ src/Saluditos/Saludos.java
@@ -0,0 +1,17 @@
+package Saluditos;
+
+import java.util.Scanner;
+
+public class Saludos {
+
+	public static void main(String args[]) {
+		
+		System.out.println("Buenos dias Ceinmark, soy Jacobo Sanjurjo");
+		
+		Scanner ent= new Scanner(System.in);
+		
+		System.out.println("Introduce un número para averigur si es primo o no");
+		
+		
+	}
+}
